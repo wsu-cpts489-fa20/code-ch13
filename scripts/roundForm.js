751e6db998d738a407059fb911e245a541dc2e97
Chris Hundhausen <chundhau@gmail.com>
2020-02-11 15:37:57 -0800

    finish

--- scripts/roundForm.js
+++ scripts/roundForm.js
@@ -6,7 +6,13 @@ class RoundForm extends React.Component {
       //store date as ISO string
       this.state = {date:  today.toISOString().substr(0,10),
                     course: '',
-                    type: "practice"}; 
+                    type: "practice",
+                    holes: "18",
+                    strokes: 80,
+                    minutes: 50,
+                    seconds: "00",
+                    SGS: "130:00",
+                    notes: ""}; 
     }
   
     handleChange = (event) => {
@@ -44,6 +50,41 @@ class RoundForm extends React.Component {
             <option value="practice">Practice</option>
             <option value="tournament">Tournament</option>
           </select> 
+          </label>
+          <p></p>
+          <label>
+          # Holes:
+          <select name="holes" value={this.state.holes} 
+            className="form-control form-center" onChange={this.handleChange}>
+            <option value="9">9</option>
+            <option value="18">18</option>
+          </select> 
+          </label>
+          <p></p>
+          <label># Strokes:
+          <input name="strokes" className="form-control form-center" type="number" 
+            min="9" max="200" value={this.state.strokes} 
+            onChange={this.handleChange} />
+          </label>
+          <p></p>
+          <label>Time: <br></br>
+          <input name="minutes" type="number" size="3"
+            min="10" max="400" value={this.state.minutes}
+            onChange={this.handleChange} />:  
+          <input name="seconds" type="number" size="2"
+            min="0" max="60" value={this.state.seconds} 
+            onChange={this.handleChange} />
+          </label>
+          <p></p>
+          <label>Speedgolf Score: <br></br>
+              <input name="SGS" className="form-center" type="text" size="6" 
+                disabled={true} value={this.state.SGS} />
+          </label>
+          <p></p>
+          <label>Notes:
+              <textarea name="notes" className="form-control" rows="6" cols="75" 
+                placeholder="Enter round notes" value={this.state.notes} 
+                onChange={this.handleChange} />
           </label>
           <p></p>
           <input type="submit" className="btn btn-primary" value="Submit" />
