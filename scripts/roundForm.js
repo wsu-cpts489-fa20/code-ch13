ac9f706a341190e1e9b224503932e89090a2a03a
Chris Hundhausen <chundhau@gmail.com>
2020-02-10 16:49:02 -0800

    ReactRoundForm1

--- scripts/roundForm.js
+++ scripts/roundForm.js
@@ -23,27 +23,6 @@ class RoundForm extends React.Component {
       return (
         <form onSubmit={this.handleSubmit}>
           <center>
-            <label>
-              Date:
-              <input name="date" className="form-control form-center" 
-                type="date" value={this.state.date} onChange={this.handleChange} />
-            </label>
-            <p></p>
-            <label>
-              Course:
-              <input name="course" className="form-control form-center" type="text"
-                value={this.state.course} onChange={this.handleChange}
-                placeholder="Course played" size="50" maxLength="50" />
-            </label>
-          <p></p>
-          <label>Type:
-          <select name="type" value={this.state.type} 
-            className="form-control form-center" onChange={this.handleChange}>
-            <option value="practice">Practice</option>
-            <option value="tournament">Tournament</option>
-          </select> 
-          </label>
-          <p></p>
           <input type="submit" className="btn btn-primary" value="Submit" />
           </center>
         </form>
