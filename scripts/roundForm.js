2152f1bce3f68bb2c61a93e19a289d3cf2cf50eb
Chris Hundhausen <chundhau@gmail.com>
2020-02-10 16:12:05 -0800

    start

---
+++ scripts/roundForm.js
@@ -0,0 +1,57 @@
+class RoundForm extends React.Component {
+    constructor(props) {
+      super(props);
+      //Create date object for today, taking time zone into consideration
+      let today = new Date(Date.now()-(new Date()).getTimezoneOffset()*60000);
+      //store date as ISO string
+      this.state = {date:  today.toISOString().substr(0,10), 
+                    course: "",
+                    type: "practice"}; 
+    }
+  
+    handleChange = (event) => {
+      const name = event.target.name;
+      this.setState({[name]: event.target.value});
+    }
+  
+    handleSubmit = (event) => {
+      alert('The form was submitted!');
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <center>
+            <label>
+              Date:
+              <input name="date" className="form-control form-center" 
+                type="date" value={this.state.date} onChange={this.handleChange} />
+            </label>
+            <p></p>
+            <label>
+              Course:
+              <input name="course" className="form-control form-center" type="text"
+                value={this.state.course} onChange={this.handleChange}
+                placeholder="Course played" size="50" maxLength="50" />
+            </label>
+          <p></p>
+          <label>Type:
+          <select name="type" value={this.state.type} 
+            className="form-control form-center" onChange={this.handleChange}>
+            <option value="practice">Practice</option>
+            <option value="tournament">Tournament</option>
+          </select> 
+          </label>
+          <p></p>
+          <input type="submit" className="btn btn-primary" value="Submit" />
+          </center>
+        </form>
+      );
+    }
+}
+
+  ReactDOM.render(
+    <RoundForm />,
+    document.getElementById('root')
+  );
